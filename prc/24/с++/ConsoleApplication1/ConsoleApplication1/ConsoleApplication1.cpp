4f510f9e17c4d1ef21d243ab79ff5fdf40661e66
nik <iljastalin@gmail.com>
2020-11-19 19:35:33 +0300

    задание 24

---
+++ prc/24/с++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+int main()
+
+{
+    "userId": 1,
+        "id" : 1,
+        "title" : "задача",
+        "completed" : false
+}
+{
+    setlocale(LC_ALL, "ru")
+    json j_object = { {"one", 1}, {"two", 2} };
+    json j_array = { 1,2,3,4,5,6,7, 8,9,10 };
+    for (auto& a : j_object.items())
+    {
+        std::cout << "key: " << a.key() << ", value: " << a.value() << '\n';
+    }
+    for (auto& a : j_array.items())
+    {
+        std::cout << "key: " << a.key() << ", value: " << a.value() << '\n';
+    }
+}
