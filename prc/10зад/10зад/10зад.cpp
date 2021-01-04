38862febce38ccbc5bdc119cdb2fa0f2e9fc5dc1
Your Name <nikita12koval>
2021-01-04 18:04:43 +0300

    10 задание

---
+++ prc/10зад/10зад/10зад.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    using namespace std;
+    int s, l1, r1, l2, r2;
+    int x1=1, x2=2;
+
+    cin >> s >> l1 >> r1 >> l2 >> r2;
+    if (pow(-10, 15) <= s, l1, r1, l2, r2 <= pow(10, 15), l1 <= r1, l2 <= r2)
+    {
+        cout << x1 << x2;
+    }
+    else
+    {
+        cout << -1;
+    }
+    
+   
+    
+    
+        
+}
