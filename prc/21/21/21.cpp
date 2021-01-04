11dbed848c554ed5ba6739c0e6bb3f8937578e24
Your Name <nikita12koval>
2021-01-04 19:02:11 +0300

    21 задание

---
+++ prc/21/21/21.cpp
@@ -0,0 +1,40 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    double a, b;
+    cout << "введите массу человека";
+    cin >> a;
+    cout << "введите рост человека";
+    cin >> b;
+    double r = pow(b, 2);
+    double c=a/r;
+    double t = a / r;
+    double x= a / r;
+    double v= a / r;
+
+    if (c < 18)
+    {
+        cout << "недостаточная масса" << " " << c;
+    }
+    else(t < 25 && t < !18);
+    {
+        cout << "нормальная масса" << " " << t;
+    }
+
+    if (x = 25 && x < 30)
+    {
+        cout << "избыточная масса" << " " << x;
+
+    }
+    else (v >= 30);
+    {
+        cout << "ожирение" << " " << v;
+    }
+
+}
+
