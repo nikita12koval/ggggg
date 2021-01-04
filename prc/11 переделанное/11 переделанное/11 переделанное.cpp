cc189110e70d98ed5da371699ddb28508dc62c3e
Your Name <nikita12koval>
2021-01-04 18:14:45 +0300

    11 задание переделанное

---
+++ prc/11 переделанное/11 переделанное/11 переделанное.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+
+{
+	setlocale(LC_ALL, "ru");
+	int a, b;
+	cout << "Введите число"<<endl;
+	cin >> a;
+	cout << "Введите степень числа" << endl;
+	cin >> b;
+	int c = pow(a, b);
+	cout << c;
+}
+
