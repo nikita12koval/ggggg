3db6c2ac670a5bac2508537b1a20ed9e4d20ff28
Your Name <nikita12koval>
2021-01-11 11:03:21 +0300

    код к первой лабораторной на с++

---
+++ prc/зад 24 переделанное/зад 24 переделанное/зад 24 переделанное.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+int main()
+{
+    json j_object = { {"one", 1}, {"two", 2} };
+    json j_array = { 1, 2, 4, 8, 16 };
+    for (auto& x : j_object.items())
+    {
+        std::cout << "key: " << x.key() << ", value: " << x.value() << '\n';
+    }
+    for (auto& x : j_array.items())
+    {
+        std::cout << "key: " << x.key() << ", value: " << x.value() << '\n';
+    }
+}
