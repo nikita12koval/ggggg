85c6fd8c78df7d9b6ee08c2981c051b405902942
Your Name <nikita12koval>
2021-03-13 17:22:23 +0300

    33z

---
+++ prc/зад 33/33/33/33.cpp
@@ -0,0 +1,46 @@
+﻿ #include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+void BubbleSort(int*, int);
+
+int main()
+{
+     int N ;
+     cout << "Razmer massiva";
+     cin >> N;
+    int *array=new int[N];
+
+    srand(time(NULL));
+    for (int i = 0; i < N; ++i) {
+        array[i] = rand() % 100;
+        cout << array[i] << ' ';
+    }
+    BubbleSort(array, N);
+
+    cout << endl << endl;
+    for (int i = 0; i < N; ++i) {
+        cout << array[i] << ' ';
+    }
+
+    cout << endl;
+    return 0;
+}
+
+
+void BubbleSort(int* array, int N)
+{
+    int temp;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return;
+}
